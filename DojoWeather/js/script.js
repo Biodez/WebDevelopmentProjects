9501f19cefcc2ad88a438d5a030351d299dddf3d
Afeez <aoladokun@my.apsu.edu>
2021-09-05 23:08:04 -0600

    changing the css file

--- DojoWeather/js/script.js
+++ DojoWeather/js/script.js
@@ -1,21 +1,21 @@
-let acceptCookie = document.getElementById("accept")
+let acceptCookie = document.getElementById("accept");
 
-acceptCookie.addEventListener('click', function(){
-    document.getElementById("cookie-nav").style.display = 'none'
-})
+acceptCookie.addEventListener("click", function () {
+  document.getElementById("cookie-nav").style.display = "none";
+});
 
 function tempConvert(element) {
-    let values = document.querySelectorAll(".tempConvert")
-    console.log(element.value)
-    if(element.value === 'farenheit') {
-        for (let i = 0; i < values.length; i++) {
-            let ctoFahr = (values[i].innerText * 9 / 5 + 32);
-            values[i].innerText = ctoFahr;
-        }
-    } else if (element.value === 'celcius') {
-        for (let i = 0; i < values.length; i++) {
-            let farToC = Math.floor((values[i].innerText - 32) * 5 / 9)
-            values[i].innerText = farToC
-        }
+  let values = document.querySelectorAll(".tempConvert");
+  console.log(element.value);
+  if (element.value === "farenheit") {
+    for (let i = 0; i < values.length; i++) {
+      let ctoFahr = (values[i].innerText * 9) / 5 + 32;
+      values[i].innerText = ctoFahr;
     }
+  } else if (element.value === "celcius") {
+    for (let i = 0; i < values.length; i++) {
+      let farToC = Math.floor(((values[i].innerText - 32) * 5) / 9);
+      values[i].innerText = farToC;
+    }
+  }
 }
